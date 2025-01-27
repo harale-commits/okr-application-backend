09b7c536bc139dbfe46458d3490b18a53fdb6ba6
harale-commits <harale150@gmail.com>
2025-01-27 13:57:44 +0530

    feat : add faker

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
+import {faker} from '@faker-js/faker';
+
 const app = express();
-const port = 8000;
+const PORT = 3000;
+
+const MOCK_OBJECTIVES = [{
+    id: faker.string.uuid(),
+    title: faker.company.buzzNoun(),
+    keyResults: {
+        title: faker.company.buzzNoun(),
+        initialValue: faker.number.int({min: 1, max: 90}),
+        currentValue: faker.number.int({min: 1, max: 90}),
+        targetValue: faker.number.int({min: 90, max: 100}),
+        metrics: faker.company.catchPhrase()
+    }
+}]
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.status(200).json(MOCK_OBJECTIVES);
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     return console.log("hello world");
 });
